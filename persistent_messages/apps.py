58aabc1845a23aed5acce9665fb070acb9576847
Hugo Rodger-Brown <hugorodgerbrown@users.noreply.github.com>
2023-04-22 16:31:25 +0100

    Initial commit

---
+++ persistent_messages/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MyAppConfig(AppConfig):
+    name = "persistent_messages"
+    verbose_name = "Poetry Template App"
---
+++ tests/test_persistent_messages.py
@@ -0,0 +1,4 @@
+def test_import() -> None:
+    import persistent_messages  # noqa: F401
+
+    assert "Empty test to ensure pytest passes."
