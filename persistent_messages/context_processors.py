a335b14a0a4c3fa77458c186c07eb5eee4034219
Hugo Rodger-Brown <hugo@yunojuno.com>
2023-05-02 15:29:09 +0100

    Fix typing in context processors

--- persistent_messages/context_processors.py
+++ persistent_messages/context_processors.py
@@ -1,17 +1,19 @@
 from typing import Callable
 
-from django.contrib.messages.storage.base import Message
 from django.http import HttpRequest
 
+from .models import PersistentMessage
 from .shortcuts import get_all_messages, get_persistent_messages
 from .templatetags.persistent_message_tags import serialize_messages
 
 
-def persistent_messages(request: HttpRequest) -> dict[str, Callable[[], list[Message]]]:
+def persistent_messages(
+    request: HttpRequest,
+) -> dict[str, Callable[[], list[PersistentMessage]]]:
     """Return just the persistent messages."""
     return {"persistent_messages": lambda: get_persistent_messages(request)}
 
 
-def all_messages(request: HttpRequest) -> dict[str, Callable[[], list[Message]]]:
+def all_messages(request: HttpRequest) -> dict[str, Callable[[], list[dict]]]:
     """Return contrib.messages and persistent_messages combined (and serialized)."""
     return {"all_messages": lambda: serialize_messages(get_all_messages(request))}
